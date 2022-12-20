fb491d1b11c62daf797ef306f457c68f01e176cb
Abdoul Kader <linkkader14@gmail.com>
2022-12-20 01:23:22 +0100

    1

--- lib/core/params/constants.dart
+++ lib/core/params/constants.dart
@@ -5,7 +5,7 @@ const kApiBaseUrl2 = 'https://api.intra.42.fr';
 const kApiBaseUrl = 'https://api.intra.42.fr/v2';
 String kSignInEndpoint = "https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-21237aece88c955fa32f7df74461d0e7837a51545d840c40bc97f42e2fa26d5f&redirect_uri=https%3A%2F%2Fzanime.app&response_type=code";
 String kRedirectUrl = "https://zanime.app";
-String kSecret = "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";
+String kSecret = "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";
 String kClientId = "u-s4t2ud-21237aece88c955fa32f7df74461d0e7837a51545d840c40bc97f42e2fa26d5f";
 
 const String kDioErrorTag = "onError";
